19be9f9341a91e7f16af852edf66c130ff02e0d0
JO Brickley <me@jobrickley.com>
2025-03-13 09:46:25 -0700

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![allow(dead_code)]  // TODO Get rid of this nonsense.
+#![allow(dead_code)] // TODO Get rid of this nonsense.
 
 #[cfg(test)]
 mod tests;
@@ -14,44 +14,47 @@ trait Chromosome {
 }
 
 impl Chromosome for NoteVec {
-
     fn breed(&self, _other: Self) -> Self {
-	todo!();
+        todo!();
     }
 
     fn display(&self) {
-	todo!();
+        todo!();
     }
 
     fn fitness(&self) -> isize {
+        // Passed &params; get with Clap
+        let target: NoteVec = vec![(49, 0), (53, 0), (56, 0)];
+        let p_steps: i8 = 4;
+        let p_notes: i8 = 3;
 
-	// Passed &params; get with Clap
-	let target: NoteVec = vec![(49,0), (53,0), (56,0)];
-	let p_steps: i8 = 4;
-	let p_notes: i8 = 3;
+        let mut steps: isize = 0;
+        let mut notes: isize = 0;
+        self[0].0;
+        for i in 0..self.len() {
+            steps = steps + (target[i].0 - self[i].0).abs() as isize;
+            notes = notes + 1;
+        }
 
-	let mut steps: isize = 0;
-	let mut notes: isize = 0;
-	self[0].0;
-	for i in 0..self.len() {
-	    steps = steps + (target[i].0 - self[i].0).abs() as isize;
-	    notes = notes + 1;
-	}
+        // These expressions make sure that the fitness of a pattern
+        // increases as it approaches the parameters, then decreases at
+        // the same rate it increased at as it grows beyond the target
+        // values.  I know they are ugly, and I am sorry.
+        steps = ((p_steps as isize)
+            - (((p_steps as isize) * (steps / (p_steps as isize)) - 1).abs()
+                + (steps % (p_steps as isize))))
+            .abs();
+        notes = ((p_notes as isize)
+            - (((p_notes as isize) * (notes / (p_notes as isize)) - 1).abs()
+                + (notes % (p_notes as isize))))
+            .abs();
 
-	// These expressions make sure that the fitness of a pattern
-	// increases as it approaches the parameters, the decreases at
-	// the same rate it increased as it grows beyond the target
-	// values.  I know they are ugly, and I am sorry.
-	steps = ((p_steps as isize) - (((p_steps as isize) * (steps / (p_steps as isize)) - 1).abs() + (steps % (p_steps as isize)))).abs();
-	notes = ((p_notes as isize) - (((p_notes as isize) * (notes / (p_notes as isize)) - 1).abs() + (notes % (p_notes as isize)))).abs();
-
-	steps + notes
+        steps + notes
     }
 
     fn mutate(self) {
-	todo!();
+        todo!();
     }
-
 }
 
 struct Population {
@@ -60,29 +63,27 @@ struct Population {
 }
 
 impl Population {
-
     fn generate_spontaneously(self) {
-	todo!();
+        todo!();
     }
 
     fn lottery_selection(&self) -> NoteVec {
-	todo!();
+        todo!();
     }
 
     /// Calculate the population's `fitness` parameter.
     fn calc_fitness(self) {
-	// Rather than calculating this, maybe have a `fitness_numer`
-	// and `fitness_denom` in the `Population` struct.  Then there
-	// could be `fitness()` function for `Population` that would
-	// just do the division when called.  The values would need to
-	// be updated each time a population member was polled.
-	todo!();
+        // Rather than calculating this, maybe have a `fitness_numer`
+        // and `fitness_denom` in the `Population` struct.  Then there
+        // could be `fitness()` function for `Population` that would
+        // just do the division when called.  The values would need to
+        // be updated each time a population member was polled.
+        todo!();
     }
 
     fn evolve(self) {
-	todo!();
+        todo!();
     }
-
 }
 
 fn main() {
